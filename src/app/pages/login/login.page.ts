fc01a1ab52dcb31d2702906840321edc3ecd3f95
cosy <kelvin.morenog28@gmail.com>
2024-10-15 14:55:14 -0300

    Cambio de dise;o y usabilidad de ../login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -12,76 +12,78 @@ import { AuthfireBaseService } from 'src/app/services/authfire-base.service';
 })
 export class LoginPage implements OnInit {
 
-  contrasenia: string = "";
-  usuario: string = "";
+  username: string = "";
+  password: string = "";
 
-  constructor(private router:Router, private menu:MenuController,private alertController: AlertController,private storage: NativeStorage, private bd: ServicioBDService,private authFirebase: AuthfireBaseService ) {}
+  constructor(private router: Router, private menu: MenuController, private alertController: AlertController, private storage: NativeStorage, private dbService: ServicioBDService, private authFirebase: AuthfireBaseService) {}
 
   ngOnInit() {
     this.menu.enable(false);
   }
 
-  async irPagina() {
-    if (this.usuario == "" || this.contrasenia == "") {
-
+  async goToPage() {
+    if (this.username == "" || this.password == "") {
       const alert = await this.alertController.create({
-        header: 'Campos Vacios',
-        message: 'Por favor intente de nuevo',
+        header: 'Empty Fields',
+        message: 'Please try again',
         buttons: ['OK'],
-        cssClass: 'estilo-alertas'
+        cssClass: 'alert-style'
       });
       await alert.present();
-
-    } else if (this.usuario == "admin" && this.contrasenia == "admin") {
-
+    } else if (this.username == "admin" && this.password == "admin") {
       this.router.navigate(['/homeadmin']);
     } else {
-
       try {
-        let CredencialFireBase = await this.authFirebase.inicioSesion(this.usuario, this.contrasenia);
+        let firebaseCredential = await this.authFirebase.inicioSesion(this.username, this.password);
         
-        if (CredencialFireBase) {
-
-          // Verificamos el usuario en la base de datos
-          let ValidarUsuario = await this.bd.BuscarCorreoUsuario(this.usuario);
+        if (firebaseCredential) {
+          // Verify user in the database
+          let validatedUser = await this.dbService.BuscarCorreoUsuario(this.username);
   
-          if (ValidarUsuario) {
-            await this.bd.modificarContra(this.contrasenia, ValidarUsuario.id_usuario);
+          if (validatedUser) {
+            await this.dbService.modificarContra(this.password, validatedUser.id_usuario);
 
-            // Guardar los datos del usuario en el NativeStorage
-            await this.storage.setItem('usuario', ValidarUsuario.id_usuario);
+            // Save user data in NativeStorage
+            await this.storage.setItem('username', validatedUser.id_usuario);
   
-            // Redirigir al home
+            // Redirect to home
             this.router.navigate(['/home']);
           } else { 
             const alert = await this.alertController.create({
-              header: 'Error al iniciar sesión',
-              message: 'Usuario o contraseña incorrectos, por favor intente de nuevo.',
+              header: 'Login Error',
+              message: 'Incorrect username or password, please try again.',
               buttons: ['OK'],
-              cssClass: 'estilo-alertas'
+              cssClass: 'alert-style'
             });
             await alert.present();
           }
-        } else{
+        } else {
           const alert = await this.alertController.create({
-            header: 'Error al iniciar sesión',
-            message: 'Usuario o contraseña incorrectos, por favor intente de nuevo.',
+            header: 'Login Error',
+            message: 'Incorrect username or password, please try again.',
             buttons: ['OK'],
-            cssClass: 'estilo-alertas'
+            cssClass: 'alert-style'
           });
           await alert.present();
         }
       } catch (error) {
-
-        // Si ocurre algún error (en Firebase o en la base de datos)
+        // Handle any errors (in Firebase or database)
         const alert = await this.alertController.create({
-          header: 'Error al iniciar sesión',
-          message: 'Usuario o contraseña incorrectos, por favor intente de nuevo.',
+          header: 'Login Error',
+          message: 'An error occurred, please try again.',
           buttons: ['OK'],
-          cssClass: 'estilo-alertas'
+          cssClass: 'alert-style'
         });
         await alert.present();
       }
     }
+  }
+
+  goToRegister(){
+    this.router.navigate(['/register']);
+  }
+
+  goToStart(){
+    this.router.navigate(['/start']);
   }
 }
