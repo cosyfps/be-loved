a9f9b4de96c08422d85724e1c82759c96c8d632d
cosy <kelvin.morenog28@gmail.com>
2024-10-15 20:08:24 -0300

    Update ../adminhome

--- src/app/pagesadm/homeadmin/homeadmin.page.ts
+++ src/app/pagesadm/homeadmin/homeadmin.page.ts
@@ -9,22 +9,21 @@ import { MenuController } from '@ionic/angular';
 })
 export class HomeadminPage implements OnInit {
 
-  nombre: string = "";
-  contrasenia: string = "";
-
   constructor(private router: Router, private activedrouter: ActivatedRoute , private menu: MenuController) { 
-    // Subscribirnos a la lectura de los parametros
-    this.activedrouter.queryParams.subscribe(param =>{
-      //valido si viene o no informacion en la ruta
-      if(this.router.getCurrentNavigation()?.extras.state){
-        this.nombre = this.router.getCurrentNavigation()?.extras?.state?.['nom'];
-        this.contrasenia = this.router.getCurrentNavigation()?.extras?.state?.['com'];
-      }
-    }) 
   }
 
   ngOnInit() {
     this.menu.enable(false);
+  }
+  
+  goToLogin(){
+    this.router.navigate(['login']);
+  }
+
+  // TODO: crear metodo isAdmin()
+
+  goToUser(){
+    this.router.navigate(['/usuarios']);
   }
 
 }
--- src/app/services/servicio-bd.service.ts
+++ src/app/services/servicio-bd.service.ts
@@ -24,8 +24,9 @@ export class DatabaseService {
   // User table
   userTable: string = "CREATE TABLE IF NOT EXISTS Usuario (id_user INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, username VARCHAR NOT NULL UNIQUE, password VARCHAR NOT NULL, email VARCHAR NOT NULL, user_photo TEXT, id_role_fk INTEGER NOT NULL, FOREIGN KEY (id_role_fk) REFERENCES rol (id_rol));";
 
-  insertUserAdmin1: string = "INSERT or IGNORE INTO Usuario(id_user, username, password, email, user_photo, id_role_fk) VALUES (1, 'cosyfps', 'KM_2024*Cl', 'kel.moreno@duocuc.cl', '', 1)";
-  insertUser1: string = "INSERT or IGNORE INTO Usuario(id_user, username, password, email, user_photo, id_role_fk) VALUES (2, 'b3hidalgo', 'b3njA*2024', 'be.hidalgog@duocuc.cl', '', 2)";
+  insertAdmin: string = "INSERT or IGNORE INTO Usuario(id_user, username, password, email, user_photo, id_role_fk) VALUES (1, 'admin', 'admin', 'admin@duocuc.cl', '', 2)";
+  insertUser1: string = "INSERT or IGNORE INTO Usuario(id_user, username, password, email, user_photo, id_role_fk) VALUES (2, 'cosyfps', 'KM_2024*Cl', 'kel.moreno@duocuc.cl', '', 2)";
+  insertUser2: string = "INSERT or IGNORE INTO Usuario(id_user, username, password, email, user_photo, id_role_fk) VALUES (3, 'b3hidalgo', 'b3njA*2024', 'be.hidalgog@duocuc.cl', '', 2)";
 
   // Variables to store query data from tables
   roleList = new BehaviorSubject([]);
@@ -90,8 +91,9 @@ export class DatabaseService {
       // Execute default inserts if they exist
       await this.database.executeSql(this.insertRoleAdmin, []);
       await this.database.executeSql(this.insertRoleUser, []);
-      await this.database.executeSql(this.insertUserAdmin1, []);
+      await this.database.executeSql(this.insertAdmin, []);
       await this.database.executeSql(this.insertUser1, []);
+      await this.database.executeSql(this.insertUser2, []);
 
       // Call the function to select data from the tables
       this.listUsers();
